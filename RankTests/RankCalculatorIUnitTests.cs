9deafa74eab91ed336f00f109f986f99f04b29f0
MariaSneg <mashakotik2090@gmail.com>
2025-05-22 10:09:36 +0300

    убрала комментарии

--- RankTests/RankCalculatorIUnitTests.cs
+++ RankTests/RankCalculatorIUnitTests.cs
@@ -7,15 +7,13 @@ public class RankCalculatorUnitTests
     private readonly RankCalculatorService _calculator = new();
     public static TheoryData<string, double> RankTestData => new TheoryData<string, double>
     {
-        { "Hello, world!", 3.0 / 13 },   // ',' and '!'
-        { "12345", 1.0 },                // All non-letters
-        { "abcDEF", 0.0 },               // All letters
-        { "", 0.0 },                     // Empty string
-        { "  a b  ", 5.0 / 7 },          // 4 spaces, 3 letters
-        { "!@#ABC", 3.0 / 6 },           // 3 symbols, 3 letters
-        { "     ", 1.0 },                // Only spaces
-        { "😊", 1.0 },                   // Emoji
-        { "New\nLine", 1.0 / 8 }         // Newline is non-letter
+        { "Hello, world!", 3.0 / 13 },   
+        { "12345", 1.0 },             
+        { "abcDEF", 0.0 },       
+        { "", 0.0 },          
+        { "     ", 1.0 },         
+        { "😊", 1.0 },          
+        { "New\nLine", 1.0 / 8 }    
     };
     
 
